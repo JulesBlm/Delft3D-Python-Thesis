8b6c77d47283c81e3812a6888dbd47c556b705a9
JulesBlm <julesblom@gmail.com>
2020-04-07 01:26:08 +0200

    bugfix

--- JulesD3D/processNetCDF.py
+++ JulesD3D/processNetCDF.py
@@ -1,8 +1,8 @@
 # TODO: READ ABOUT OOP
 # turn this into a class
-# turn seperate functions into classes?
+# turn seperate fudatasettions into classes?
 # leave it as is?
-# TODO: Use ufuncs for vector sums
+# TODO: Use ufudatasets for vector sums
 
 from JulesD3D.processing_2d import vector_sum
 import xarray as xr
@@ -10,8 +10,8 @@ import numpy as np
 from os import path
 
 # def magnitude(a, b):
-#     func = lambda x, y: np.sqrt(x ** 2 + y ** 2)
-#     return xr.apply_ufunc(func, a, b)
+#     fudataset = lambda x, y: np.sqrt(x ** 2 + y ** 2)
+#     return xr.apply_ufudataset(fudataset, a, b)
 
 def fixCORs(dataset):
     '''
@@ -29,10 +29,10 @@ def fixCORs(dataset):
     return dataset
 
 def makeMeshGrid(length=45000, width=18000, x_gridstep=300, y_gridstep=300):
-#     print("length:", length)
-#     print("width:", width)
-#     print("x_gridstep:", x_gridstep)
-#     print("y_gridstep", y_gridstep)
+    # print("length:", length)
+    # print("width:", width)
+    # print("x_gridstep:", x_gridstep)
+    # print("y_gridstep", y_gridstep)
     
     first_x_center = int(x_gridstep/2)
     xList = [0] + [i for i in range(first_x_center, int(width) + 1 * int(x_gridstep), int(x_gridstep))] #prepend a zero
@@ -51,15 +51,15 @@ def fixMeshGrid(dataset, mystery_flag=False):
     '''
     Derives gridsteps and dimensions from passed DataSet
     Assumes uniform grid, curved grid wont work!
-    References to XZ and YZ need to be passed explicitly because Dask loads the netCDF lazily
+    Reference to XZ and YZ need to be passed explicitly because Dask loads the netCDF lazily
     The mystery flag is a Boolean because sometimes 1 and sometimes 2 gridsteps need to be subtracted from the length ¯\_(ツ)_/¯ , don't know why
     '''
     print("------ Fixing mesh grid, assuming a uniform grid ------")
     dataset.XZ.load()
     dataset.YZ.load()    
     
-    x_gridstep = dataset.XZ[2][-1] - dataset.XZ[1][-1]
-    y_gridstep = dataset.YZ[-2][-1] - dataset.YZ[-2][-2]
+    x_gridstep = dataset.XZ.values[2][-1] - dataset.XZ.values[1][-1]
+    y_gridstep = dataset.YZ.values[-2][-1] - dataset.YZ.values[-2][-2]
     
     width = (dataset.XZ.shape[0]-2) * x_gridstep
     if mystery_flag:
@@ -104,7 +104,7 @@ def addDepth(dataset):
     dataset.coords['depth'] = depth
     
     # doesnt work since some update of xarray it think
-    # nc = dataset.rename_dims({'SIG_INTF': 'KMAXOUT'}) # rename SIG_INTF to KMAXOUT [breaks since some xarray update, I forgot why it was necessary]
+    # dataset = dataset.rename_dims({'SIG_INTF': 'KMAXOUT'}) # rename SIG_INTF to KMAXOUT [breaks sidatasete some xarray update, I forgot why it was necessary]
     
     ##### depth at cell centers #####
     depth_center = dataset.SIG_LYR @ dataset.DPS
@@ -113,53 +113,53 @@ def addDepth(dataset):
     depth_center = depth_center.assign_attrs({"unit": "m", "long_name": "Depth at Sigma-layer centers"})
 
     dataset.coords['depth_center'] = depth_center
-    # nc = dataset.rename_dims({'SIG_LYR': 'KMAXOUT_RESTR'}) # rename SIG_LYR to KMAXOUT_RESTR        
+    # dataset = dataset.rename_dims({'SIG_LYR': 'KMAXOUT_RESTR'}) # rename SIG_LYR to KMAXOUT_RESTR        
 
     ##### Add layer thickness DataArray to Data #####
     layer_thickness = np.diff(-depth.values)
-    nc['layer_thickness'] = (depth_center.dims, layer_thickness)
-    nc['layer_thickness'].attrs = {'long_name': 'Sigma-layer thickness', 'units': 'm'}
+    dataset['layer_thickness'] = (depth_center.dims, layer_thickness)
+    dataset['layer_thickness'].attrs = {'long_name': 'Sigma-layer thickness', 'units': 'm'}
     
-    return nc
+    return dataset
 
 def addUnderlayerCoords(dataset):
     '''
     Add underlayer coordinates to these data variables, which is nice for interactive plotting with Holoviews/hvPlot
     '''
     
-    nc['MSED'] = dataset.MSED.assign_coords(nlyr=dataset.nlyr.values)
-    nc['LYRFRAC'] = dataset.LYRFRAC.assign_coords(nlyr=dataset.nlyr.values)
-    nc['DP_BEDLYR'] = dataset.DP_BEDLYR.assign_coords(nlyrp1=dataset.nlyrp1.values)
+    dataset['MSED'] = dataset.MSED.assign_coords(nlyr=dataset.nlyr.values)
+    dataset['LYRFRAC'] = dataset.LYRFRAC.assign_coords(nlyr=dataset.nlyr.values)
+    dataset['DP_BEDLYR'] = dataset.DP_BEDLYR.assign_coords(nlyrp1=dataset.nlyrp1.values)
     
-    nc['nlyrp1'].attrs = {'standard_name': 'Interfaces of underlayers', 'long_name': 'Number of interfaces of underlayers'}
-    nc['nlyr'].attrs = {'standard_name': 'Number of underlayers', 'long_name': 'Number of underlayers'}
+    dataset['nlyrp1'].attrs = {'standard_name': 'Interfaces of underlayers', 'long_name': 'Number of interfaces of underlayers'}
+    dataset['nlyr'].attrs = {'standard_name': 'Number of underlayers', 'long_name': 'Number of underlayers'}
     
-    return nc
+    return dataset
 
 # This only works for sets that don;t have nested values
-def addVectorSum(nc, U_comp, V_comp, key="summed", attrs={}, dims=('time', 'M', 'N')):
+def addVectorSum(dataset, U_comp, V_comp, key="summed", attrs={}, dims=('time', 'M', 'N')):
     '''Adds summed DataArray to new key in DataSet and drops components from DataSet'''
-    summed = vector_sum(nc[U_comp].values, nc[V_comp].values)
-    nc[key] = (dims, summed)
-    nc[key].attrs = attrs
+    summed = vector_sum(dataset[U_comp].values, dataset[V_comp].values)
+    dataset[key] = (dims, summed)
+    dataset[key].attrs = attrs
         
-    nc = dataset.drop_vars([U_comp, V_comp])
+    dataset = dataset.drop_vars([U_comp, V_comp])
 
-    return nc
+    return dataset
 
 def makeVelocity(dataset): # , U1, V1 directly pass component values
     # Make Horizontal velocity sum per layer
     velocity_sum = vector_sum(dataset.U1.values, dataset.V1.values) # Velocity per layer
-    nc['velocity'] = (('time',  'KMAXOUT_RESTR', 'M', 'N',), velocity_sum)
-    nc['velocity'].attrs = {'long_name': 'Horizontal velocity per layer', 'units': 'm/s', 'grid': 'grid', 'location': 'edge1'}
+    dataset['velocity'] = (('time',  'KMAXOUT_RESTR', 'M', 'N',), velocity_sum)
+    dataset['velocity'].attrs = {'long_name': 'Horizontal velocity per layer', 'units': 'm/s', 'grid': 'grid', 'location': 'edge1'}
     
     # why am i doing this again? For pyvista right?
-    nc['velocity']  = dataset.velocity.transpose('time', 'M', 'N', 'KMAXOUT_RESTR', transpose_coords=False)
-    nc['velocity'] = dataset.velocity.assign_coords(depth=(('time', 'M', 'N', 'KMAXOUT_RESTR'), nc['depth_center'].values)) # for pyvista?
+    dataset['velocity']  = dataset.velocity.transpose('time', 'M', 'N', 'KMAXOUT_RESTR', transpose_coords=False)
+    dataset['velocity'] = dataset.velocity.assign_coords(depth=(('time', 'M', 'N', 'KMAXOUT_RESTR'), dataset['depth_center'].values)) # for pyvista?
     
-    return nc
+    return dataset
 
-def makeVectorSumsSediments(nc, sediment_dicts=[]):
+def makeVectorSumsSediments(dataset, sediment_dicts=[]):
     '''
     Loops of all constituents (sediments) found in DataSet and sums their vector components and add them to DataSet,
     sediment_dicts is a list of dicts; each dict should have these keys
@@ -179,21 +179,21 @@ def makeVectorSumsSediments(nc, sediment_dicts=[]):
         for datavar in sediment_dicts:
             U, V = datavar['U_V_keys']
             
-            if U in nc and V in nc:
+            if U in dataset and V in dataset:
                 new_summed_key = f"{datavar['new_key']}_{sediment[11:-1]}" # TODO: bad because this expects all constituents to be prefixed with sediment but who cares only i use this junk
                 print("Adding to DataSet with key:", new_summed_key)
                 key['attrs']['long_name'] = f"{kdatavarey['attrs']['long_name']} {sediment[2:-1]}"
                 # print("New long name:", datavar['attrs']['long_name'])
                 print(f"Summing {U} and {V} for {sediment}")
-                nc = addVectorSum(nc, U, V, key=new_summed_key, attrs=datavar['attrs'], dims=key['dims'])
+                dataset = addVectorSum(dataset, U, V, key=new_summed_key, attrs=datavar['attrs'], dims=key['dims'])
             else:
                 print(f"⚠️ Keywords {U} and {V} are not present in given DataSet ⚠️")
-                return nc
+                return dataset
     
     print("Done adding summed DataArrays for ", *(sed[2:-1] for sed in sediments), "to DataSet")    
-    return nc
+    return dataset
 
-def dropJunk(nc, drop_list=[
+def dropJunk(dataset, drop_list=[
          'SBUU', 'SBVV',
          'SSVV', 'SSUU',
          'TAUKSI', 'TAUETA',
@@ -219,20 +219,20 @@ def dropJunk(nc, drop_list=[
     #                 'TAUETA', 'TAUMAX', 'UMNLDF', 'VMNLDF', 'SBUUA', 'SBVVA', 'SSUUA', 'SSVVA', 'MORFAC', 'MFTAVG', 'MORAVG', 'TAUETA',
     #                            'R1', 'DG', 'MORFT', 'DM', 'VICUV', 'DPU0', 'DPV0', 'WPHY', 'DMSEDCUM', 'LYRFRAC', 'MSED', 'W', 'WS', 'RICH', 'RTUR1'], errors='ignore')
 
-    #     dataset_clean.load().to_netcdf('clean_compressed_sampleD3D.nc', mode='w', engine='netcdf4', format='NETCDF4')     
+    #     dataset_clean.load().to_netcdf('clean_compressed_sampleD3D.dataset', mode='w', engine='netcdf4', format='NETCDF4')     
     
     # Remove component DataArrays from DataSet
-    print("Dropping a bunch of DataArrays from DataSet...", end='')
+    print("Dropping a budataseth of DataArrays from DataSet...", end='')
     
-    nc = dataset.drop_vars(drop_list, errors='ignore')    
+    dataset = dataset.drop_vars(drop_list, errors='ignore')    
     print('Done dropping variables.')    
 
-    return nc
+    return dataset
 
-# WORK IN PROGRESS: need to figure out how to pass DataSet as argument maintaining reference, ie allowing functions to access DataArrays in passed DataSet
-def processNetCDF(nc, mystery_flag=True, bottom_stress=True, sum_sediments=True, sum_velocities=True, drop_junk=True):
+# WORK IN PROGRESS: need to figure out how to pass DataSet as argument maintaining referedatasete, ie allowing fudatasettions to access DataArrays in passed DataSet
+def processNetCDF(dataset, mystery_flag=True, bottom_stress=True, sum_sediments=True, sum_velocities=True, drop_junk=True):
     '''
-    Chains all the nc processing steps
+    Chains all the dataset processing steps
     Add drop_list and sediment_dicts as keyword arguments
     '''
     
@@ -255,43 +255,43 @@ def processNetCDF(nc, mystery_flag=True, bottom_stress=True, sum_sediments=True,
     bottom_stres_dims = ('time', 'M', 'N')
     
     # The processing chain
-    nc = fixMeshGrid(nc, dataset.XZ.values, dataset.YZ.values, mystery_flag=mystery_flag)
-    print("● Fixed mesh grid") # find a way to nicely chain these. just mutate nc for sake of ease?    
-    nc = addDepth(dataset) # needs to be done BEFORE makeVelocity!
+    dataset = fixMeshGrid(dataset, dataset.XZ.values, dataset.YZ.values, mystery_flag=mystery_flag)
+    print("● Fixed mesh grid") # find a way to nicely chain these. just mutate dataset for sake of ease?    
+    dataset = addDepth(dataset) # needs to be done BEFORE makeVelocity!
     print("● Added depth & depth_center to DataSet")    
-    nc = addVectorSum(nc, 'TAUKSI', 'TAUETA', key="bottom_stress", attrs=bottom_stress_attrs, dims=bottom_stres_dims) if bottom_stress else print("● Skipping bottom stress")
-    nc = makeVectorSumsSediments(nc, sediment_dicts=sediment_vect_component) if sum_sediments else print("● Skippin¡g sediment sums")
-    nc = makeVelocity(dataset) if sum_velocities  else print("● Skipping velocity sum") # dataset.U1.values, dataset.V1.values
+    dataset = addVectorSum(dataset, 'TAUKSI', 'TAUETA', key="bottom_stress", attrs=bottom_stress_attrs, dims=bottom_stres_dims) if bottom_stress else print("● Skipping bottom stress")
+    dataset = makeVectorSumsSediments(dataset, sediment_dicts=sediment_vect_component) if sum_sediments else print("● Skippin¡g sediment sums")
+    dataset = makeVelocity(dataset) if sum_velocities  else print("● Skipping velocity sum") # dataset.U1.values, dataset.V1.values
     print("● Summed horizontal velocity")
-    nc = addUnderlayerCoords(dataset)
+    dataset = addUnderlayerCoords(dataset)
     print("● Assigned underlayer coordinates")
-    nc['bottom_diff'] = dataset.DPS.isel(time=0) - dataset.DPS
+    dataset['bottom_diff'] = dataset.DPS.isel(time=0) - dataset.DPS
     print("● Made accumulated deposition/erosion")
-    nc = dropJunk(dataset)
+    dataset = dropJunk(dataset)
 
-    return nc
+    return dataset
     
 # add flags for what vector sums to write
 # dict for each vector sum? [{'dims': , 'attrs': , 'key': '', }
-def writeCleanCDF(ncfilename, chunks=10, mystery_flag=False):
+def writeCleadatasetDF(datasetfilename, chunks=10, mystery_flag=False):
     '''
     Add vector sum for velocities and sediment transport DataArrays to DataSet
     Remove useless stuff and save new netCDF to disk
     TODO: Whats a good chunk number?
     '''
     
-    with xr.open_dataset(ncfilename, chunks={'time': chunks}) as nc:
-        nc = fixMeshGrid(nc, dataset.XZ.values, dataset.YZ.values, mystery_flag=True)
-        nc = addDepth(dataset)
-        nc = makeVelocity(dataset)
-#         nc = makeBottomStress(dataset)
-#         nc = addVectorSum(nc, 'TAUKSI', 'TAUETA', key="bottom_stress", attrs=bottom_stress_attrs, dims=bottom_stres_dims)
+    with xr.open_dataset(datasetfilename, chunks={'time': chunks}) as dataset:
+        dataset = fixMeshGrid(dataset, dataset.XZ.values, dataset.YZ.values, mystery_flag=True)
+        dataset = addDepth(dataset)
+        dataset = makeVelocity(dataset)
+#         dataset = makeBottomStress(dataset)
+#         dataset = addVectorSum(dataset, 'TAUKSI', 'TAUETA', key="bottom_stress", attrs=bottom_stress_attrs, dims=bottom_stres_dims)
         print("Calculated bottom stress sum")
-        nc = addUnderlayerCoords(dataset)
-        nc = dropJunk(dataset)
+        dataset = addUnderlayerCoords(dataset)
+        dataset = dropJunk(dataset)
         print("Dropped variables from DataSet")
         
-        root, ext = path.splitext(ncfilename)
+        root, ext = path.splitext(datasetfilename)
         new_filename = root + '_clean' + ext
         
         # TODO overwrite old NetCDF so we don't have to calculate vector sums again U SURE THO?
